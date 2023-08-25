a327c2bcd4157af3aa08485ca8ce7b8b4ef6e428
alexanderyanthar <alexander.yanthar@outlook.com>
2023-08-25 11:11:58 -0400

    finished styling page and made it responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 const Header = () => {
   return (
     <header className="App-header">
+      <div className="wrapper header-text">
         <h1>Trader's Compass</h1>
         <h2>Unlock the Market's Secrets with Trader's Compass - Your All-in-One Trading Navigator</h2>
+      </div>
       </header>
   )
 }
