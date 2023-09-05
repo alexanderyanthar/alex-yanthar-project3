5493148d8bac9f343760b23ab3c76bb7feb3b322
alexanderyanthar <alexander.yanthar@outlook.com>
2023-09-04 22:51:44 -0400

    added error handling to app

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,31 @@ import Footer from './components/Footer';
 import MainContent from './components/MainContent';
 
 function App() {
-  const [selectedCurrencyPair, setSelectedCurrencyPair] = useState('EUR/USD')
-  const [chartData, setChartData] = useState([]);
+  const [selectedCurrencyPair, setSelectedCurrencyPair] = useState('EUR/USD');
+  const [chartData, setChartData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   const apiKey = 'GEDZHAFMQCTPRQZN';
 
   const currencyPairs = ['EUR/USD', 'USD/CAD', 'USD/JPY', 'AUD/USD', 'GBP/USD', 'NZD/USD', 'USD/CHF'];
 
   const fetchData = async (currencyPair) => {
+    setLoading(true);
+    setError(null);
+
     try {
       const response = await fetch(
         `https://www.alphavantage.co/query?function=FX_DAILY&from_symbol=${currencyPair.substring(0, 3)}&to_symbol=${currencyPair.substring(4)}&outputsize=full&apikey=${apiKey}`
       );
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      
       const data = await response.json();
+
+      if (data['Time Series FX (Daily)'] === undefined) {
+        throw new Error('API data limit reached. Wait 1 Minute and reload.');
+      }
 
       const chartData = Object.entries(data['Time Series FX (Daily)']).map(([date, values]) => {
         const open = parseFloat(values['1. open']);
@@ -35,8 +48,11 @@ function App() {
       });
 
       setChartData(chartData);
-    } catch (e) {
-      console.error('Error fetching data:', e);
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setError(error);
+      setLoading(false);
     }
   };
 
@@ -47,11 +63,17 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <MainContent 
-        chartData={chartData}
-        selectedCurrencyPair={selectedCurrencyPair}
-        currencyPairs={currencyPairs}
-        setSelectedCurrencyPair={setSelectedCurrencyPair} />
+      {loading && <div>Loading...</div>}
+      {error && <div>Error: {error.message}</div>}
+      {chartData === null && !loading && !error && <div>API data limit reached. Wait 1 minute before reloading.</div>}
+      {chartData && !loading && !error && (
+        <MainContent
+          chartData={chartData}
+          selectedCurrencyPair={selectedCurrencyPair}
+          currencyPairs={currencyPairs}
+          setSelectedCurrencyPair={setSelectedCurrencyPair}
+        />
+      )}
       <Footer />
     </div>
   );
